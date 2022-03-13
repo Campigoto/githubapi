76cd2b4991df65cc38f924bbcd3eb5d66f0aadd3
Evandro Campigoto <campigoto@hotmail.com>
2022-03-13 18:56:40 -0300

    Search

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,19 +1,64 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
+
+
+type FormData = {
+  login: string;
+};
+
+type Profile =
+{
+  url: string;
+  name: string;
+  followers: string;
+  location: string;
+  avatar_url: string;
+};
 
 const CepSearch = () => {
+
+
+  const [profile, setProfile] = useState<Profile>();
+  const [formData, setFormData] = useState<FormData>({
+    login: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+  axios
+      .get(`https://api.github.com/users/${formData?.login}`)
+      .then((response) => {
+        setProfile(response.data);
+      })
+      .catch((error) => {
+        setProfile(undefined);
+      });
+  };
+
   return (
     <div className="cep-search-container">
-      <h1 className="text-primary">Busca CEP</h1>
+      <h1 className="text-primary">Encontre um perfil Github</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="login"
+              value={formData?.login}
               className="search-input"
-              placeholder="CEP (somente números)"
-              onChange={() => {}}
+              placeholder="Usuário Github"
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
@@ -21,9 +66,17 @@ const CepSearch = () => {
           </div>
         </form>
 
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
-
+        {profile && (
+          <>
+        <ResultCard title="Perfil: " description={profile.url} />
+        <ResultCard title="Seguidores: " description={profile.followers} />
+        <ResultCard title="Localidade: " description={profile.location} />
+        <ResultCard title="Nome: " description={profile.name} />
+        <div className="card-top-container">
+                <img src={profile.avatar_url} alt="Foto" />
+            </div>
+        </> 
+        )}
       </div>
     </div>
   );
